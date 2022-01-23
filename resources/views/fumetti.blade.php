033b5512658fb77407b31b146bb27ff2c1980649
Antonioangy <dumitruangy26@gmail.com>
2022-01-23 23:48:49 +0100

    html+css pagina principale

---
+++ resources/views/fumetti.blade.php
@@ -0,0 +1,166 @@
+@extends('layouts.main-layout')
+@section('content')
+<div id="container">
+
+    <div id="current-series">CURRENT SERIES</div>
+
+    <div class="serie">
+
+        <div class="serie-img">
+            <img src="https://media-assets.wired.it/photos/6194c3683aee792659b58fe6/2:3/w_900,h_1350,c_limit/FDo0mohVQAAP5Td.jpeg">
+        </div>
+
+        <div class="serie-name">
+            <span>Spider-Man</span>
+        </div>
+    
+    </div>
+
+    <div class="serie">
+
+        <div class="serie-img">
+            <img src="https://media-assets.wired.it/photos/6194c3683aee792659b58fe6/2:3/w_900,h_1350,c_limit/FDo0mohVQAAP5Td.jpeg">
+        </div>
+
+        <div class="serie-name">
+            <span>Spider-Man</span>
+        </div>
+    
+    </div>
+    <div class="serie">
+
+        <div class="serie-img">
+            <img src="https://media-assets.wired.it/photos/6194c3683aee792659b58fe6/2:3/w_900,h_1350,c_limit/FDo0mohVQAAP5Td.jpeg">
+        </div>
+
+        <div class="serie-name">
+            <span>Spider-Man</span>
+        </div>
+    
+    </div>
+    <div class="serie">
+
+        <div class="serie-img">
+            <img src="https://media-assets.wired.it/photos/6194c3683aee792659b58fe6/2:3/w_900,h_1350,c_limit/FDo0mohVQAAP5Td.jpeg">
+        </div>
+
+        <div class="serie-name">
+            <span>Spider-Man</span>
+        </div>
+    
+    </div>
+    <div class="serie">
+
+        <div class="serie-img">
+            <img src="https://media-assets.wired.it/photos/6194c3683aee792659b58fe6/2:3/w_900,h_1350,c_limit/FDo0mohVQAAP5Td.jpeg">
+        </div>
+
+        <div class="serie-name">
+            <span>Spider-Man</span>
+        </div>
+    
+    </div>
+    <div class="serie">
+
+        <div class="serie-img">
+            <img src="https://media-assets.wired.it/photos/6194c3683aee792659b58fe6/2:3/w_900,h_1350,c_limit/FDo0mohVQAAP5Td.jpeg">
+        </div>
+
+        <div class="serie-name">
+            <span>Spider-Man</span>
+        </div>
+    
+    </div>
+    <div class="serie">
+
+        <div class="serie-img">
+            <img src="https://media-assets.wired.it/photos/6194c3683aee792659b58fe6/2:3/w_900,h_1350,c_limit/FDo0mohVQAAP5Td.jpeg">
+        </div>
+
+        <div class="serie-name">
+            <span>Spider-Man</span>
+        </div>
+    
+    </div>
+    <div class="serie">
+
+        <div class="serie-img">
+            <img src="https://media-assets.wired.it/photos/6194c3683aee792659b58fe6/2:3/w_900,h_1350,c_limit/FDo0mohVQAAP5Td.jpeg">
+        </div>
+
+        <div class="serie-name">
+            <span>Spider-Man</span>
+        </div>
+    
+    </div>
+    <div class="serie">
+
+        <div class="serie-img">
+            <img src="https://media-assets.wired.it/photos/6194c3683aee792659b58fe6/2:3/w_900,h_1350,c_limit/FDo0mohVQAAP5Td.jpeg">
+        </div>
+
+        <div class="serie-name">
+            <span>Spider-Man</span>
+        </div>
+    
+    </div>
+    <div class="serie">
+
+        <div class="serie-img">
+            <img src="https://media-assets.wired.it/photos/6194c3683aee792659b58fe6/2:3/w_900,h_1350,c_limit/FDo0mohVQAAP5Td.jpeg">
+        </div>
+
+        <div class="serie-name">
+            <span>Spider-Man</span>
+        </div>
+    
+    </div>
+    <div class="serie">
+
+        <div class="serie-img">
+            <img src="https://media-assets.wired.it/photos/6194c3683aee792659b58fe6/2:3/w_900,h_1350,c_limit/FDo0mohVQAAP5Td.jpeg">
+        </div>
+
+        <div class="serie-name">
+            <span>Spider-Man</span>
+        </div>
+    
+    </div>
+    <div class="serie">
+
+        <div class="serie-img">
+            <img src="https://media-assets.wired.it/photos/6194c3683aee792659b58fe6/2:3/w_900,h_1350,c_limit/FDo0mohVQAAP5Td.jpeg">
+        </div>
+
+        <div class="serie-name">
+            <span>Spider-Man</span>
+        </div>
+    
+    </div>
+    
+    <div id="btn-more">LOAD MORE</div> 
+</div> 
+{{-- Shop --}}
+<section id="blue-bar">
+    <div class="buy-comics" >
+      <img src="https://play-lh.googleusercontent.com/fHr2pe1B7n_dvrFX3e-P-BFrsiMJ-nPh4_wn4Yj2vwlINS_Lb4CwK8qKGK8upu5to-RK">
+      <span>DIGITAL COMICS</span>
+    </div>
+    <div class="buy-comics" >
+        <img src="https://play-lh.googleusercontent.com/fHr2pe1B7n_dvrFX3e-P-BFrsiMJ-nPh4_wn4Yj2vwlINS_Lb4CwK8qKGK8upu5to-RK">
+        <span>DC MERCHANDISE</span>
+      </div>
+      <div class="buy-comics" >
+        <img src="https://play-lh.googleusercontent.com/fHr2pe1B7n_dvrFX3e-P-BFrsiMJ-nPh4_wn4Yj2vwlINS_Lb4CwK8qKGK8upu5to-RK">
+        <span>SUBSCRIPTION</span>
+      </div>
+      <div class="buy-comics" >
+        <img src="https://play-lh.googleusercontent.com/fHr2pe1B7n_dvrFX3e-P-BFrsiMJ-nPh4_wn4Yj2vwlINS_Lb4CwK8qKGK8upu5to-RK">
+        <span>COMIC SHOP LOCATOR</span>
+      </div>
+      <div class="buy-comics" >
+        <img src="https://play-lh.googleusercontent.com/fHr2pe1B7n_dvrFX3e-P-BFrsiMJ-nPh4_wn4Yj2vwlINS_Lb4CwK8qKGK8upu5to-RK">
+        <span>DC POWER VISA</span>
+      </div>
+  </section>
+@endsection
